8c6b6397507cc2a40f41ab08fad223f3c828e0d4
Vilash V <vilash@programmr.com>
2016-10-17 21:10:49 +0530

    fixed sorting

--- src/VillieBlogBundle/Repository/PostRepository.php
+++ src/VillieBlogBundle/Repository/PostRepository.php
@@ -10,4 +10,8 @@ namespace VillieBlogBundle\Repository;
  */
 class PostRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function findAll()
+    {
+        return $this->findBy(array(), array('publishedAt' => 'DESC'));
+    }
 }
